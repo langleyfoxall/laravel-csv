e742a3448daee9622dfe2b99f521f19168e55586
Ashley Heywood <ash@codebased.co.uk>
2026-07-07 11:05:14 +0100

    make small refactors

--- src/Exceptions/UnableToReadFile.php
+++ src/Exceptions/UnableToReadFile.php
@@ -6,8 +6,6 @@ class UnableToReadFile extends CsvException
 {
     public static function fromDisk(string $path, ?string $disk): self
     {
-        $diskName = $disk ?? 'default';
-
-        return new self("Unable to read CSV file [{$path}] from disk [{$diskName}].");
+        return new self("Unable to read CSV file [{$path}] from disk [".($disk ?? 'default').'].');
     }
 }
